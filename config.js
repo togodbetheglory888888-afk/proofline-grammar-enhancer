80fc118dd03a8926d98aa2177919f08db27a75aa
CHRISTIAN NOLASCO <togodbetheglory888888@gmail.com>
2026-09-16 15:16:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-/* Proofline V7 license configuration.
-   Set apiUrl to your deployed license API before selling.
-   Example: https://your-worker.example.workers.dev
-*/
-window.PROOFLINE_LICENSE_CONFIG = {
-  apiUrl: "",
-  requireOnlineActivation: false,
-  product: "PROOFLINE-GRAMMAR-49"
-};
+(function () {
+  "use strict";
+
+  window.PROOFLINE_LICENSE_CONFIG = {
+    apiUrl: "",
+    requireOnlineActivation: false,
+    product: "PROOFLINE-GRAMMAR-49"
+  };
+})();
